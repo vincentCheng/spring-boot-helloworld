f7a9a634e90c46501bd37118baee38cdcbed413b
huanggang <462256272@qq.com>
2023-09-06 17:21:12 +0800

    init

---
+++ src/main/java/com/heima/helloworld_01_01_quick/Helloworld0101QuickApplication.java
@@ -0,0 +1,13 @@
+package com.heima.helloworld_01_01_quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloworld0101QuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Helloworld0101QuickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heima/helloworld_01_01_quick/Helloworld0101QuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heima.helloworld_01_01_quick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helloworld0101QuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
